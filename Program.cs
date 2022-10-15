8984c77116d8d7eef52bd3969d00c54f092ac451
Ilchan <annilchen@yandex.ru>
2022-10-15 16:05:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+bool Wha;
+string f;
+double a, b, c, d;
+Console.WriteLine("Введите 4 числа");
+do
+{
+    f = Console.ReadLine();
+    Wha = double.TryParse(f, out a);
+    if (Wha == false)
+    {
+        Console.WriteLine("Введите Число.");
+    }
+}
+while (Wha == false);
+do
+{
+    f = Console.ReadLine();
+    Wha = double.TryParse(f, out b);
+    if (Wha == false)
+    {
+        Console.WriteLine("Введите Число.");
+    }
+}
+while (Wha == false);
+do
+{
+    f = Console.ReadLine();
+    Wha = double.TryParse(f, out c);
+    if (Wha == false)
+    {
+        Console.WriteLine("Введите Число.");
+    }
+}
+while (Wha == false);
+do
+{
+    f = Console.ReadLine();
+    Wha = double.TryParse(f, out d);
+    if (Wha == false)
+    {
+        Console.WriteLine("Введите Число.");
+    }
+}
+while (Wha == false);
+if((a == b) && (a == c) && (a == d))
+{
+    Console.WriteLine($"Числа равны");
+}    
+else
+{
+    Console.WriteLine("Числа не равны");
+}
